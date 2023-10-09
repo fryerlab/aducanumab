a3e9fca998608b71e42ae983114362e6b2671105
John Fryer, Ph.D <106278420+fryerlab@users.noreply.github.com>
2023-10-09 13:32:21 -0700

    Update 03_sample_loop.sh

--- scRNA/scripts/01_cellranger/03_sample_loop.sh
+++ scRNA/scripts/01_cellranger/03_sample_loop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for sample in 705A_Adu_Male 734A_Adu_Male 736B_Adu_Female 738A_Adu_Female 738B_Adu_Female 823A_IgG_Male 851A_IgG_Male 939A_IgG_Female 939B_IgG_Female 959B_IgG_Female 
+for sample in 705A_Adu_Male 734A_Adu_Male 823A_IgG_Male 851A_IgG_Male
 do
 	qsub count.sh $sample
 done
