73d0cdb955e6654e86d0f2749bf1bfbe61b9ad0a
John Fryer, Ph.D <106278420+fryerlab@users.noreply.github.com>
2023-10-09 13:32:42 -0700

    Update 04_get_web_summary.sh

--- scRNA/scripts/01_cellranger/04_get_web_summary.sh
+++ scRNA/scripts/01_cellranger/04_get_web_summary.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # extract webs summaries
-for sample in 705A_Adu_Male 734A_Adu_Male 736B_Adu_Female 738A_Adu_Female 738B_Adu_Female 823A_IgG_Male 851A_IgG_Male 939A_IgG_Female 939B_IgG_Female 959B_IgG_Female 
+for sample in 705A_Adu_Male 734A_Adu_Male 823A_IgG_Male 851A_IgG_Male
 do
 	cd /aducanumab/count/$sample/outs
 	cp web_summary.html ../../web_summaries/"$sample"_web_summary.html
