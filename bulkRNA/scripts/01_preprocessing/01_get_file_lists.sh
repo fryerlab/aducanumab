64c939bc19742d55e77b9cdca2c3bd3b23051412
John Fryer, Ph.D <106278420+fryerlab@users.noreply.github.com>
2023-10-09 13:44:08 -0700

    Update 01_get_file_lists.sh

--- bulkRNA/scripts/01_preprocessing/01_get_file_lists.sh
+++ bulkRNA/scripts/01_preprocessing/01_get_file_lists.sh
@@ -3,11 +3,11 @@
 # Get fastq file list
 # There are 8 fastq files per sample
 # Each sample has R1 and R2 on all 4 lanes
-cd /research/labs/neurology/fryer/projects/aducanumab/mouse/bulkRNA
-out=/research/labs/neurology/fryer/m214960/aducanumab/bulkRNA/refs/fastq_file_list.txt
+cd /projects/aducanumab/mouse/bulkRNA
+out=/aducanumab/bulkRNA/refs/fastq_file_list.txt
 ls -1 | grep .fastq.gz > $out
 
 # Get sample file list
-cd /research/labs/neurology/fryer/projects/aducanumab/mouse/bulkRNA
-out=/research/labs/neurology/fryer/m214960/aducanumab/bulkRNA/refs/sample_file_list.txt
+cd /projects/aducanumab/mouse/bulkRNA
+out=/aducanumab/bulkRNA/refs/sample_file_list.txt
 ls -1 | grep _L001_R1_001.fastq.gz > $out
